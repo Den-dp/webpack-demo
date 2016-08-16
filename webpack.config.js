69faa495a4b198e297830e55921524b071521b44
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2016-08-16 20:20:02 +0300

    chore(webpack-dev-server): define reusable code for configuring hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
 
+const parts = require('./utils/parts');
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
@@ -35,7 +37,14 @@ switch(process.env.npm_lifecycle_event) {
     config = merge(common, {});
     break;
   default:
-    config = merge(common, {});
+    config = merge(
+      common,
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
 module.exports = validate(config);
