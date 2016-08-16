f7515a408d9fe230f529e23851ea9892a979622e
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2016-08-16 19:56:50 +0300

    chore(webpack): integrating webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -37,4 +38,4 @@ switch(process.env.npm_lifecycle_event) {
     config = merge(common, {});
 }
 
-module.exports = config;
+module.exports = validate(config);
